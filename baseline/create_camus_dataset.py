7c42098ea5697515391378a3534323507db70b9c
matteotaf <tafuromatteo00@gmail.com>
2022-09-19 16:36:20 +0200

    Update create_camus_dataset to contain a Dataset with 2CH+4CH

--- baseline/create_camus_dataset.py
+++ baseline/create_camus_dataset.py
@@ -20,6 +20,9 @@ train_2ch_masks_list = sorted(os.listdir(train_path + "2ch/masks/"))
 train_4ch_frames_list = sorted(os.listdir(train_path + "4ch/frames/"))
 train_4ch_masks_list = sorted(os.listdir(train_path + "4ch/masks/"))
 
+train_all_frames_list = sorted(os.listdir(train_path + "2ch/frames/") + os.listdir(train_path + "4ch/frames/"))
+train_all_masks_list = sorted(os.listdir(train_path + "2ch/masks/") + os.listdir(train_path + "4ch/masks/"))
+
 # Create hierarchical h5py file, ready to be filled with 4 datasets
 f = h5py.File("../data/image_dataset.hdf5", "w")
 
@@ -34,6 +37,14 @@ f.create_dataset("train 4ch frames", (900, 384, 384, 1),
 
 f.create_dataset("train 4ch masks", (900, 384, 384, 1),
                 chunks = (4, 384, 384, 1), dtype = "int32")
+
+f.create_dataset("train all frames", (1800, 384, 384, 1),
+                chunks = (4, 384, 384, 1), dtype = "float32")
+
+f.create_dataset("train all masks", (1800, 384, 384, 1),
+                chunks = (4, 384, 384, 1), dtype = "int32")
+
+
 
 # Iterate over images and save them into the corresponding dataset
 j = 0
@@ -72,7 +83,27 @@ for i in train_4ch_masks_list:
         new_array = cv2.resize(array[0,:,:], dsize=(384, 384), interpolation=cv2.INTER_NEAREST)
         new_array = np.reshape(new_array,(384,384,1))
         f["train 4ch masks"][j,...] = new_array[...]
+        j = j + 1
+
+j = 0
+for i in train_all_frames_list:
+    if "mhd" in i:
+        array = mhd_to_array(os.path.join(train_path, f"{'2ch' if '2CH' in i else '4ch'}/frames", i))
+        new_array = cv2.resize(array[0,:,:], dsize=(384, 384), interpolation=cv2.INTER_CUBIC)
+        new_array = np.reshape(new_array,(384,384,1))
+        new_array = new_array/255
+        f["train all frames"][j,...] = new_array[...]
+        j = j + 1        
+        
+j = 0
+for i in train_all_masks_list:
+    if "mhd" in i:
+        array = mhd_to_array(os.path.join(train_path, f"{'2ch' if '2CH' in i else '4ch'}/masks", i))
+        new_array = cv2.resize(array[0,:,:], dsize=(384, 384), interpolation=cv2.INTER_NEAREST)
+        new_array = np.reshape(new_array,(384,384,1))
+        f["train all masks"][j,...] = new_array[...]
         j = j + 1       
+
         
 f.close()
 
